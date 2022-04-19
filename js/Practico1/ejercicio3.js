17da0a39695295e61ef75c4fff05e46d88a5db31
GuidoPisarra <guidopisarra@gmail.com>
2022-04-19 19:57:05 -0300

    practico 1

--- js/Practico1/ejercicio3.js
+++ js/Practico1/ejercicio3.js
@@ -5,16 +5,44 @@ let canvas = document.querySelector("#myCanvas");
 let ctx = canvas.getContext("2d");
 let canvasWidth = canvas.width;
 let canvasHeight = canvas.height;
+let imageData = ctx.createImageData(canvasWidth, canvasHeight);
 
 
-for (let index = 0; index < 1000; index++) {
-    ctx.fillStyle = rgba(index);
-    ctx.fillRect(0,index,canvasWidth,canvasHeight);
+let r = 0;
+let g = 0;
+let b = 0;
+let a = 255;
+function drawRect(imageData, r, g, b, a) {
+
+    for (let x = 0; x < canvasWidth; x++) {
+        let r;
+        let g;
+        let b;
+        let a = 255;
+        for (let x=0; x<canvasWidth; x++){
+            
+        }
+        
+        for (let y = 0; y < canvasHeight; y++) {
+            let coeficiente= 255 / (canvasWidth);
+            r = y * coeficiente;
+            g = y * coeficiente;
+            b = y* coeficiente;
+            setPixel(imageData, x, y, r, g, b, a);
+        }
+
+    }
 }
-function rgba(indice) {
-    let r = (0 + indice) / 4;
-    let g = (0 + indice) / 4 ;
-    let b = (0 + indice) / 4 ;
-    let a = 1;
-    return `rgba(${r}, ${g}, ${b}, ${a})`;
+            
+
+
+function setPixel(imageData, x, y, r, g, b, a) {
+    let index = (x + y * imageData.width) * 4;
+    imageData.data[index + 0] = r;
+    imageData.data[index + 1] = g;
+    imageData.data[index + 2] = b;
+    imageData.data[index + 3] = a;
 }
+
+drawRect(imageData, r, g, b, a);
+ctx.putImageData(imageData, 0, 0);
